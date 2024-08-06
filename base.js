643875110df1d28cbd86ea2e97080a1c27d1d1bb
Sudiksha <patels3@etown.edu>
2024-08-06 13:39:07 -0400

    got rid of banner, fixed overlap

--- base.js
+++ base.js
@@ -4,9 +4,6 @@ const graphics=canvas.getContext("2d");
 
 let orderMode = true;
 
-baseName();
-tortilla();
-rice();
 
 
 
@@ -15,36 +12,20 @@ rice();
 // }
 
 
-function rice(){
-    // graphics.fillStyle ="darkgrey";
-    // graphics.beginPath();
-    // graphics.arc(95,70, 25, 0, Math.PI*2);
-    // graphics.fill();
-    // graphics.closePath();
-}
 
-function tortilla(){
-    // graphics.fillStyle = "yellow";
-    // graphics.beginPath();
-    // graphics.arc(220,80, 45, 0, Math.PI*2);
-    // graphics.fill();
-    // graphics.closePath();
-}
+// function baseName(){
+//     graphics.fillStyle ="maroon"; 
+//     graphics.fillRect(0,0,70,20);
 
+//     graphics.fillStyle = "rgb(245, 240, 229)";
+//     graphics.beginPath();
+//     graphics.lineTo(70,0);
+//     graphics.lineTo(60,10);
+//     graphics.lineTo(70,20);
+//     graphics.fill();
+//     graphics.closePath();
 
-function baseName(){
-    graphics.fillStyle ="maroon"; 
-    graphics.fillRect(0,0,70,20);
-
-    graphics.fillStyle = "rgb(245, 240, 229)";
-    graphics.beginPath();
-    graphics.lineTo(70,0);
-    graphics.lineTo(60,10);
-    graphics.lineTo(70,20);
-    graphics.fill();
-    graphics.closePath();
-
-    let titleName = "BASE!";
-    graphics.fillText(titleName, 20, 12);
+//     let titleName = "BASE!";
+//     graphics.fillText(titleName, 20, 12);
     
-}
+// }
