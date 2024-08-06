533132abdbfa3ca0b6c68051cb85fde39dda1a9b
Vinaya <doshiv@etown.edu>
2024-08-06 11:41:25 -0400

    delete

--- start.js
+++
@@ -1,5 +0,0 @@
-
-    document.getElementById('startGameButton').addEventListener('click', function() {
-    window.location.href = 'order.htm'; 
-    // Replace 'game.html' with the URL of the page you want to link to
-});
