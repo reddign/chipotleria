47579e772169da284496c6b60e2ec7320b310f98
Vinaya <doshiv@etown.edu>
2024-08-06 11:27:33 -0400

    update

--- start.js
+++ start.js
@@ -1,4 +1,5 @@
-document.getElementById('startGameButton').addEventListener('click', function() {
-    window.location.href = 'game.html'; 
+
+    document.getElementById('startGameButton').addEventListener('click', function() {
+    window.location.href = 'order.htm'; 
     // Replace 'game.html' with the URL of the page you want to link to
 });
