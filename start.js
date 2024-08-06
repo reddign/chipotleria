167af6b694a52dd287ec8662c949ab9f033511e0
Julia <ibrahimj@etown.edu>
2024-08-06 11:25:09 -0400

    yippee

---
+++ start.js
@@ -0,0 +1,4 @@
+document.getElementById('startGameButton').addEventListener('click', function() {
+    window.location.href = 'game.html'; 
+    // Replace 'game.html' with the URL of the page you want to link to
+});
