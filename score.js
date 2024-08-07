cd0da412c9651629034d66a6e6e39d44c85ed514
Vinaya <doshiv@etown.edu>
2024-08-07 11:24:24 -0400

    hope

---
+++ score.js
@@ -0,0 +1,24 @@
+
+    let x= Math.random()*10;
+    x= Math.round(x);
+    if(x==1){
+    alert('Ypu got $1.');
+}else if(x==2){
+    alert("You got $2.");
+}else if(x==3){
+    alert("You got $3.");
+}else if(x==4){
+    alert("You got $4.");
+}else if(x==5){
+alert("You got $5.");
+}else if(x==6){
+    alert("You got $6!");
+}else if(x==7){
+    alert("You got $7!");
+}else if(x==8){
+    alert("You got $8!");
+}else if(x==9){
+    alert("You got $9!");
+}else{
+    alert("You got $10!");
+}
