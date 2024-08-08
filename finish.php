1feb0568f0090db0acd8e8565dc37053b8528179
Julia <ibrahimj@etown.edu>
2024-08-08 12:31:53 -0400

    Fix

--- finish.php
+++ finish.php
@@ -4,36 +4,6 @@
 <link rel="stylesheet"  href="finish.css">
 <title>Mama's Chipotleria</title>
 
-<button onclick="start()">Done!</button>
-        <script>
-            function start(){
-                
-    let x= Math.random()*10;
-    x= Math.round(x);
-    if(x==1){
-    alert('Ypu got $1.');
-}else if(x==2){
-    alert("You got $2.");
-}else if(x==3){
-    alert("You got $3.");
-}else if(x==4){
-    alert("You got $4.");
-}else if(x==5){
-alert("You got $5.");
-}else if(x==6){
-    alert("You got $6!");
-}else if(x==7){
-    alert("You got $7!");
-}else if(x==8){
-    alert("You got $8!");
-}else if(x==9){
-    alert("You got $9!");
-}else{
-    alert("You got $10!");
-}
-            }
-        </script>
-        
 <!DOCTYPE html>
 
 <html>
@@ -101,4 +71,33 @@ alert("You got $5.");
         </header>
     </body>
 </html>
+<button onclick="start()">Done!</button>
+        <script>
+            function start(){
+                
+    let x= Math.random()*10;
+    x= Math.round(x);
+    if(x==1){
+    alert('Ypu got $1.');
+}else if(x==2){
+    alert("You got $2.");
+}else if(x==3){
+    alert("You got $3.");
+}else if(x==4){
+    alert("You got $4.");
+}else if(x==5){
+alert("You got $5.");
+}else if(x==6){
+    alert("You got $6!");
+}else if(x==7){
+    alert("You got $7!");
+}else if(x==8){
+    alert("You got $8!");
+}else if(x==9){
+    alert("You got $9!");
+}else{
+    alert("You got $10!");
+}
+            }
+        </script>
 
