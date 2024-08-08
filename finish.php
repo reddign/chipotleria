1eb03b16f51415dbd65cab7208cdb64e6c20f9da
Julia <ibrahimj@etown.edu>
2024-08-08 19:04:39 -0400

    edits

--- finish.php
+++ finish.php
@@ -66,6 +66,7 @@
         <!-- size of screen (keep these measurements constant)/|\ -->
         <button onclick="start()">Done!</button>
         <script>
+<<<<<<< HEAD
             const canvas = document.getElementById('canvas');
             const ctx = canvas.getContext('2d');
             const background = new Image();
@@ -75,9 +76,36 @@
             };
             background.src = 'checkout.png'; // background image
 
-            function start() {
+            function start() 
                 let x = Math.floor(Math.random() * 10) + 1;
                 alert(`You got $${x}.`);
+=======
+            function start(){
+                
+    let x= Math.random()*10;
+    x= Math.round(x);
+    if(x==1){
+    alert('You got $1.');
+}else if(x==2){
+    alert("You got $2.");
+}else if(x==3){
+    alert("You got $3.");
+}else if(x==4){
+    alert("You got $4.");
+}else if(x==5){
+alert("You got $5.");
+}else if(x==6){
+    alert("You got $6!");
+}else if(x==7){
+    alert("You got $7!");
+}else if(x==8){
+    alert("You got $8!");
+}else if(x==9){
+    alert("You got $9!");
+}else{
+    alert("You got $10!");
+}
+>>>>>>> ca0658c (edits)
             }
         </script>
     </main>
