7c4e53c65c663d14bfd35c12d3bbd2f99db673f9
Rachelle <kwitchr@etown.edu>
2024-08-08 10:10:01 -0400

    changes

--- session.php
+++ session.php
@@ -1,6 +1,14 @@
 <?php
 session_start();
-?>
 
-<?php
-$_SESSION[]
+$mysqli = new mysqli("195.35.59.14","u121755072_chipotleria","T>C~5g3u[h5","u121755072_chipotleria");
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+}
+// $userid = $_SESSION["UserID"];
+
+print_r($_SESSION);
+
+echo "SID: ".SID."<br>session_id(): ".session_id(); 
+echo "<br>COOKIE: ".$_COOKIE["PHPSESSID"];
+?>
